b5c024c525fa86b6e393a1dd66622fc69d11e9a5
Kush Panjabi <kushpanjabi@icloud.com>
2020-03-24 13:46:37 -0700

    fixes as per feedback

--- index.js
+++ index.js
@@ -1,79 +1,83 @@
+'use strict';
+
 const apiKey = 'MW9S-E7SL-26DU-VV8V';
 
 
 // Fetches the BART API data for Real Time Estimates //
 
 function getStationDepartures(stnInput) {
-    fetch(`https://api.bart.gov/api/etd.aspx?cmd=etd&key=${apiKey}&orig=${stnInput}&json=y`)
-      .then(response => response.json())
-      .then(responseJson => 
-        displayDepartures(responseJson));
-  }
+	fetch(`https://api.bart.gov/api/etd.aspx?cmd=etd&key=${apiKey}&orig=${stnInput}&json=y`)
+		.then(response => response.json())
+		.then(responseJson =>
+			displayDepartures(responseJson))
+		.catch(error => alert("Something went wrong. Try again later."));
+
+}
 
 
 // Displays the Real Time Departure information by station selected // 
 
-function displayDepartures(responseJson){
-  console.log(responseJson);
-  $('#search').empty();
-  $('#search').append(`
+function displayDepartures(responseJson) {
+	$('#search').empty();
+	$('#search').append(`
     <p class="station-name">${responseJson.root.station[0].name}<p>
     <p class="time inline">${responseJson.root.time}</p><hr>
     `)
-  
-    for (i=0; i<responseJson.root.station[0].etd.length; i++){
-    $('#search').append(`
+
+	for (let i = 0; i < responseJson.root.station[0].etd.length; i++) {
+		$('#search').append(`
       <p class="destination">${responseJson.root.station[0].etd[i].destination} <img src="images/bart-logo-sm.png" alt="small bart logo" class="small-logo"></p>
       `)
-      for (j=0; j<responseJson.root.station[0].etd[i].estimate.length; j++) {
-        $('#search').append(`
+		for (let j = 0; j < responseJson.root.station[0].etd[i].estimate.length; j++) {
+			$('#search').append(`
       <p class="${responseJson.root.station[0].etd[i].estimate[j].color} lh eta">${responseJson.root.station[0].etd[i].estimate[j].minutes} mins</p>
     `)
-    } 
-    }
-    }
+		}
+	}
+}
 
 // Fetches the BART API data for advisories //
 
 function getAdvisories() {
-  fetch(`https://api.bart.gov/api/bsa.aspx?cmd=bsa&key=${apiKey}&json=y`)
-    .then(response => response.json())
-      .then(responseJsonAdv => 
-        displayAdvisories(responseJsonAdv));
-    }
+	fetch(`https://api.bart.gov/api/bsa.aspx?cmd=bsa&key=${apiKey}&json=y`)
+		.then(response => response.json())
+		.then(responseJsonAdv =>
+			displayAdvisories(responseJsonAdv))
+		.catch(error => alert("Something went wrong. Try again later."));
+
+}
 
 
 // Displays the current BART advisories //
 
-function displayAdvisories(responseJsonAdv){
-  console.log(responseJsonAdv);
-  $('.advisory').append(`
+function displayAdvisories(responseJsonAdv) {
+	$('.advisory').append(`
   <ul>
     <li class="ls-circle"><p class=adv-text>${responseJsonAdv.root.bsa["0"].description["#cdata-section"]}</li></p>
   </ul>
   `)
-  }
+}
 
 
 // Listens for a submit button click, pulls data, and displays chosen station departure estimates //
 
-  function watchSubmit() {
-    $('.js-search-form').submit(function(event){
-      event.preventDefault();
-  
-      let userStnInput = $('.station-options').val();
+function watchSubmit() {
+	$('.js-search-form').submit(function (event) {
+		event.preventDefault();
 
-      if (userStnInput === "") {
-        alert('You must select a station!')
-      } else {
-      getStationDepartures(userStnInput);
-    }
-  });
-  }
+		const userStnInput = $('.station-options').val();
 
-  function runApp() {
-      watchSubmit();
-      getAdvisories();
-  }
+		if (userStnInput === "") {
+			alert('You must select a station!')
+		} else {
+			getStationDepartures(userStnInput);
+		}
+	});
+}
 
-  $(runApp);
+function runApp() {
+	watchSubmit();
+	getAdvisories();
+}
+
+$(runApp);
